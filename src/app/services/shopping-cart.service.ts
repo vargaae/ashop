aa3e6a74e683b917c1eab14f2f075505d8618269
vargaae <58937079+vargaae@users.noreply.github.com>
2021-07-18 01:35:59 +0200

    Clearing the Shopping Cart

--- src/app/services/shopping-cart.service.ts
+++ src/app/services/shopping-cart.service.ts
@@ -22,30 +22,11 @@ export class ShoppingCartService {
         .subscribe(p => this.product = p);
   }
 
-  private create() {
-    return this.db.list('/shopping-carts').push({
-      dateCreated: new Date().getTime()
-    });
-  }
-
   async getCart(): Promise<Observable<ShoppingCart>> {
     let cartId = await this.getOrCreateCartId();
     return this.db.object('/shopping-carts/' + cartId).valueChanges()
     .pipe(map((x: any) => new ShoppingCart(x.items)));
   }
-
-  private getItem(cartId: string, productId: string) {
-    return this.db.object('/shopping-carts/' + cartId + '/items/' + productId);
-}
-
-  private async getOrCreateCartId(): Promise<string> {
-    let cartId = localStorage.getItem('cartId');
-    if (cartId) return cartId;
-
-      let result = await this.create();
-      localStorage.setItem('cartId', result.key);
-      return result.key;
-    }
 
   async addToCart(product: Product) {
     // this.updateItem(product, 1, 1);
@@ -61,10 +42,10 @@ export class ShoppingCartService {
               price: product.price,
               quantity: (item.payload.val().quantity) + 1
             });
-            console.log(item.type);
-            console.log(item.key)
-            console.log(item.payload.val())
-            console.log(item.payload.val().quantity)
+            // console.log(item.type);
+            // console.log(item.key)
+            // console.log(item.payload.val())
+            // console.log(item.payload.val().quantity)
         } else {
             item$.update({
               title: product.title,
@@ -87,15 +68,41 @@ export class ShoppingCartService {
             price: product.price,
             quantity: (item.payload.val().quantity) - 1
           });
-          console.log(item.type);
-          console.log(item.key)
-          console.log(item.payload.val())
-          console.log(item.payload.val().quantity)
+          // console.log(item.type);
+          // console.log(item.key)
+          // console.log(item.payload.val())
+          // console.log(item.payload.val().quantity)
       }
   });
 }
 
-// async updateItem(product: Product, change: number, emptyCart: number) {
+  async clearCart() {
+    let cartId = await this.getOrCreateCartId();
+    this.db.object('/shopping-carts/' + cartId + '/items').remove();
+  }
+
+  private create() {
+    return this.db.list('/shopping-carts').push({
+      dateCreated: new Date().getTime()
+    });
+  }
+
+
+
+  private getItem(cartId: string, productId: string) {
+    return this.db.object('/shopping-carts/' + cartId + '/items/' + productId);
+}
+
+  private async getOrCreateCartId(): Promise<string> {
+    let cartId = localStorage.getItem('cartId');
+    if (cartId) return cartId;
+
+      let result = await this.create();
+      localStorage.setItem('cartId', result.key);
+      return result.key;
+    }
+
+// private async updateItem(product: Product, change: number, emptyCart: number) {
 //   let cartId = await this.getOrCreateCartId();
 //   let item$ = this.getItem(cartId, product.id);
 
