147123ed5eef1d6715e544420948029e37c372e5
vargaae <58937079+vargaae@users.noreply.github.com>
2021-07-18 14:53:39 +0200

    spread operator

--- src/app/models/shopping-cart-item.ts
+++ src/app/models/shopping-cart-item.ts
@@ -10,7 +10,10 @@
 
      constructor(init?: Partial<ShoppingCartItem>) {
        Object.assign(this, init);
+       // with Object.assign() we're copying all the properties from init into the this current object (so this is the target, and our source is init)
      }
+       // this means init can be an object that looks like the ShoppingCartItem, it can have one or more properties
+       
     // constructor(public product: Product, public quantity: number) {}
 
     get totalPrice() { return this.price * this.quantity }
