cc5dd7efdd467846d39f219f3775b02a746d5e1d
vargaae <58937079+vargaae@users.noreply.github.com>
2021-07-20 23:29:24 +0200

    Associating the Order with the Current User

--- src/app/services/order.service.ts
+++ src/app/services/order.service.ts
@@ -1,3 +1,4 @@
+import { ShoppingCartService } from './shopping-cart.service';
 import { Injectable } from '@angular/core';
 import { AngularFireDatabase } from '@angular/fire/database';
 
@@ -6,9 +7,20 @@ import { AngularFireDatabase } from '@angular/fire/database';
 })
 export class OrderService {
 
-  constructor(private db: AngularFireDatabase) { }
+  constructor(private db: AngularFireDatabase, private cartService: ShoppingCartService) { }
 
   storeOrder(order) {
-    this.db.list('/orders').push(order);
+    return this.db.list('/orders').push(order);
+  }
+
+  async placeOrder(order) {
+    // return this.db.list('/orders').push(order);
+    // this returns a promise so we're going to return this to the outside because later we want to get the key of this new order and redirect the user to the order success page
+
+    let result = await this.db.list('/orders').push(order);
+    // instead of returning the result immediately we need to await the result and store it in a variable
+    // this.cartService.clearCart();
+    // right after we store the order we should clear the shopping cart
+    return result;
   }
 }
--- src/app/shopping/components/check-out/check-out.component.ts
+++ src/app/shopping/components/check-out/check-out.component.ts
@@ -1,8 +1,11 @@
+import { AuthService } from './../../../services/auth.service';
 import { OrderService } from './../../../services/order.service';
 import { Subscription } from 'rxjs';
 import { ShoppingCart } from './../../../models/shopping-cart';
 import { ShoppingCartService } from './../../../services/shopping-cart.service';
 import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Order } from 'src/app/models/order';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'check-out',
@@ -12,25 +15,33 @@ import { Component, OnDestroy, OnInit } from '@angular/core';
 export class CheckOutComponent implements OnInit, OnDestroy {
   userId: string;
   shipping = {};
+  name?: any;
   cart: ShoppingCart;
-  subscription: Subscription;
+  cartSubscription: Subscription;
+  userSubscription: Subscription;
+  // shipping: any;
 
   constructor(
+    private authService: AuthService,
     private shoppingCartService: ShoppingCartService,
-    private orderService: OrderService
+    private orderService: OrderService,
+    private router: Router
     ) {}
 
   async ngOnInit() {
     let cart$ = await this.shoppingCartService.getCart();
-    this.subscription = cart$.subscribe(cart => this.cart = cart);
+    this.cartSubscription = cart$.subscribe(cart => this.cart = cart);
+    this.authService.user$.subscribe(user => this.userId = user.uid);
   }
 
   ngOnDestroy() {
-    this.subscription.unsubscribe();
+    this.cartSubscription.unsubscribe();
+    this.userSubscription.unsubscribe();
   }
 
   placeOrder() {
     let order = {
+      userId: this.userId,
       datePlaced: new Date().getTime(),
       shipping: this.shipping,
       items: this.cart.items.map(i => {
@@ -50,5 +61,41 @@ export class CheckOutComponent implements OnInit, OnDestroy {
 
     // console.log(this.shipping);
   }
+
+  // async placeOrder(shipping: any) {
+  //   let order = new Order(this.userId, shipping, this.cart);
+  //   // this is a refactoring of the code below, a much cleaner way
+
+  //   // let order = {
+  //   //   userId: this.userId,
+  //   //   datePlaced: new Date().getTime(),
+  //   //   shipping: this.shipping,
+  //   //   items: this.cart.items.map(i => {
+  //   //     return {
+  //   //       product: {
+  //   //         title: i.title,
+  //   //         imageUrl: i.imageUrl,
+  //   //         price: i.price
+  //   //       },
+  //   //       quantity: i.quantity,
+  //   //       totalPrice: i.totalPrice
+  //   //     };
+  //   //   })
+  //   //   // we want to get the items from the shopping cart and map them to a new structure
+  //   // };
+
+  //   // this.orderService.placeOrder(order).then(result => {
+  //   //   this.router.navigate(['/order-success', result.key]);
+  //   // });
+  //   // this is the same as the code below, the only difference is there we await for the result and that's why we need to add async in front of our method's name
+
+  //   // now that we have an order object we can call the orderService and pass this order to the placeOrder() method
+  //   let result = await this.orderService.placeOrder(order);
+
+  //   this.router.navigate(['/order-success', result.key]);
+  //   // result.key is the route parameter; we should note that this is key and not $key, $key is used when we read a node from Firebase but key is used when we store something in Firebase so Firebase returns this newly generated id in this key property
+
+
+  // }
 
 }
