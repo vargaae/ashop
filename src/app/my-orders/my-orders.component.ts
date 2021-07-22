8bdb7eb3a7d74244feb4c3d16b0e4825b6d4d36d
vargaae <58937079+vargaae@users.noreply.github.com>
2021-07-22 03:41:48 +0200

    Displaying the Orders-Start

--- src/app/my-orders/my-orders.component.ts
+++ src/app/my-orders/my-orders.component.ts
@@ -1,11 +1,29 @@
-import { Component } from '@angular/core';
-import { Router } from '@angular/router';
+import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { switchMap } from 'rxjs/operators';
+import { Order } from '../models/order';
+import { AuthService } from '../services/auth.service';
+import { OrderService } from '../services/order.service';
 
 @Component({
   selector: 'app-my-orders',
   templateUrl: './my-orders.component.html',
   styleUrls: ['./my-orders.component.css']
 })
-export class MyOrdersComponent {
+export class MyOrdersComponent implements OnInit {
+  orders$;
+
+  constructor(
+    private authService: AuthService,
+    private orderService: OrderService) {
+    // this.orders$ = authService.user$.switchMap(u => orderService.getOrdersByUser(u.uid));
+    }
+
+  ngOnInit() {
+    this.orders$ = this.authService.user$
+      .pipe(switchMap(user => {
+        return this.orderService.getOrdersByUser(user.uid);
+      }));
+  }
 
 }
--- src/app/services/order.service.ts
+++ src/app/services/order.service.ts
@@ -1,6 +1,7 @@
 import { ShoppingCartService } from './shopping-cart.service';
 import { Injectable } from '@angular/core';
 import { AngularFireDatabase } from '@angular/fire/database';
+import { map } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
@@ -21,4 +22,53 @@ export class OrderService {
     // right after we store the order we should clear the shopping cart
     return result;
   }
+
+  getOrders() {
+    return this.db.list('/orders').valueChanges();
+  }
+
+  getOrdersByUser(userId: string) {
+    return this.db.list('/orders').valueChanges();
+  }
+
+  // getOrders() {
+  //   return this.db.list('/orders').snapshotChanges()
+  //   // .pipe(map(data => {
+  //   //   return data.map(action => {
+  //   //     const $key = action.payload.key;
+  //   //     const data = { $key, ...action.payload.val() };
+  //   //     return data;
+  //   //   });
+  //   // }));
+  //   // here we simply return the list of orders from Firebase
+  // }
+
+  getOrderById(orderId: string) {
+    return this.db.object('/orders/' + orderId).valueChanges();
+  }
+
+  deleteOrder(id: string) {
+    return this.db.list('/orders/' + id).remove();
+  }
+
+  // // getOrdersByUser(userId: string) {
+  // //   return this.db.list('/orders', {
+  // //     query: {
+  // //       orderByChild: 'userId',
+  // //       equalTo: userId
+  // //     }
+  // //   });
+  // // }
+
+  // getOrdersByUser(userId: string) {
+  //   // return this.db.list('/orders', ref => ref.orderByChild('userId').equalTo(userId))
+  //     // .snapshotChanges().pipe(map(data => {
+  //     //   return data.map(action => {
+  //     //     const $key = action.payload.key;
+  //     //     const data = { $key, ...action.payload.val() };
+  //     //     return data;
+  //     //   });
+  //     // }));
+  // }
+
 }
