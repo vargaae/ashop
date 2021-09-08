76d4e7661d8cc88f347df0be3943b2f5984f70ae
vargaae <58937079+vargaae@users.noreply.github.com>
2021-09-08 17:51:21 +0200

    auth improved-emailsignedin forgot password-lazyL

--- src/app/membership/components/forgot-password/forgot-password.module.ts
+++ src/app/membership/components/forgot-password/forgot-password.module.ts
@@ -1,5 +1,6 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
 import { SharedModule } from 'src/app/shared/shared.module';
 
 import { ForgotPasswordRoutingModule } from './forgot-password-routing.module';
@@ -7,6 +8,11 @@ import { ForgotPasswordComponent } from './forgot-password.component';
 
 @NgModule({
   declarations: [ForgotPasswordComponent],
-  imports: [CommonModule, SharedModule, ForgotPasswordRoutingModule],
+  imports: [
+    CommonModule,
+    SharedModule,
+    ForgotPasswordRoutingModule,
+    ReactiveFormsModule
+  ],
 })
 export class ForgotPasswordModule {}
