fe653b52bd251602461d90401f7d9cbe1f75eebf
vargaae <58937079+vargaae@users.noreply.github.com>
2021-09-08 17:45:17 +0200

    auth improved-emailsignedin verify email-lazyL

--- src/app/membership/components/verify-email/verify-email.component.ts
+++ src/app/membership/components/verify-email/verify-email.component.ts
@@ -1,15 +1,53 @@
 import { Component, OnInit } from '@angular/core';
+import { AngularFireAuth } from '@angular/fire/auth';
+import { Router } from '@angular/router';
+import { AuthService } from 'src/app/shared/services/auth.service';
 
 @Component({
   selector: 'app-verify-email',
   templateUrl: './verify-email.component.html',
-  styleUrls: ['./verify-email.component.css']
+  styleUrls: ['./verify-email.component.css'],
 })
 export class VerifyEmailComponent implements OnInit {
+  email: string;
+  mailSent: boolean;
+  isProgressVisible: boolean;
+  firebaseErrorMessage: string;
 
-  constructor() { }
+  constructor(
+    private authService: AuthService,
+    private router: Router,
+    public afAuth: AngularFireAuth
+  ) {
+    this.email = '';
+    this.mailSent = false;
+    this.isProgressVisible = false;
 
-  ngOnInit(): void {
+    this.firebaseErrorMessage = '';
   }
 
+  ngOnInit(): void {
+    this.afAuth.authState.subscribe((user) => {
+      // if the user is logged in, update the form value with their email address
+      if (user) {
+        this.email = user.email;
+      }
+    });
+  }
+
+  resendVerificationEmail() {
+    this.isProgressVisible = true; // show the progress indicator as we start the Firebase password reset process
+
+    this.authService.resendVerificationEmail().then((result) => {
+      this.isProgressVisible = false; // no matter what, when the auth service returns, we hide the progress indicator
+      if (result == null) {
+        // null is success, false means there was an error
+        console.log('verification email resent...');
+        this.mailSent = true;
+      } else if (result.isValid == false) {
+        console.log('verification error', result);
+        this.firebaseErrorMessage = result.message;
+      }
+    });
+  }
 }
--- src/app/membership/components/verify-email/verify-email.module.ts
+++ src/app/membership/components/verify-email/verify-email.module.ts
@@ -1,17 +1,12 @@
-import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
 
+import { SharedModule } from './../../../shared/shared.module';
 import { VerifyEmailRoutingModule } from './verify-email-routing.module';
 import { VerifyEmailComponent } from './verify-email.component';
 
-
 @NgModule({
-  declarations: [
-    VerifyEmailComponent
-  ],
-  imports: [
-    CommonModule,
-    VerifyEmailRoutingModule
-  ]
+  declarations: [VerifyEmailComponent],
+  imports: [CommonModule, SharedModule, VerifyEmailRoutingModule],
 })
-export class VerifyEmailModule { }
+export class VerifyEmailModule {}
