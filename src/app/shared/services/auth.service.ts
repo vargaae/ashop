921fa483d70745a9470974dd57440814f47b8584
vargaae <58937079+vargaae@users.noreply.github.com>
2021-09-08 12:27:48 +0200

    auth improved - improved authService, User model

--- src/app/shared/services/auth.service.ts
+++ src/app/shared/services/auth.service.ts
@@ -7,20 +7,40 @@ import { AngularFireAuth } from '@angular/fire/auth';
 import * as firebase from 'firebase/app';
 import { Observable, of } from 'rxjs';
 import { switchMap } from 'rxjs/operators';
+import { AngularFirestore } from '@angular/fire/firestore';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
+      // START OF EMAIL SIGN IN
+      userLoggedIn: boolean;      // other components can check on this variable for the login status of the user
+      authState: any;
+      // END OF EMAIL SIGN IN
+
+
+// START OF GOOGLE SIGIN
   user$: Observable<firebase.default.User>;
 
   constructor(
     private userService: UserService,
     private route: ActivatedRoute,
     private router: Router,
-    private afAuth: AngularFireAuth
+    private afAuth: AngularFireAuth,
+    private afs: AngularFirestore
     ) {
       this.user$ = afAuth.authState;
+
+
+      this.userLoggedIn = false;
+
+      this.afAuth.onAuthStateChanged((user) => {              // set up a subscription to always know the login status of the user
+          if (user) {
+              this.userLoggedIn = true;
+          } else {
+              this.userLoggedIn = false;
+          }
+      });
   }
 
 loginWithGoogle() {
@@ -43,5 +63,103 @@ get appUser$() : Observable<AppUser> {
       return of(null);
     }));
   }
+
+  // END OF GOOGLE SIGN IN
+      // START OF EMAIL SIGN IN
+
+  loginUser(email: string, password: string): Promise<any> {
+    return this.afAuth.signInWithEmailAndPassword(email, password)
+        .then(() => {
+            console.log('Auth Service: loginUser: success');
+            // this.router.navigate(['/dashboard']);
+        })
+        .catch(error => {
+            console.log('Auth Service: login error...');
+            console.log('error code', error.code);
+            console.log('error', error);
+            if (error.code)
+                return { isValid: false, message: error.message };
+        });
+}
+
+ signupUser(user: any): Promise<any> {
+    return this.afAuth.createUserWithEmailAndPassword(user.email, user.password)
+        .then((result) => {
+            let emailLower = user.email.toLowerCase();
+
+            this.afs.doc('/users/' + emailLower)                        // on a successful signup, create a document in 'users' collection with the new user's info
+                .set({
+                    accountType: 'endUser',
+                    displayName: user.displayName,
+                    displayName_lower: user.displayName.toLowerCase(),
+                    email: user.email,
+                    email_lower: emailLower
+                });
+
+                result.user.sendEmailVerification();                    // immediately send the user a verification email
+        })
+        .catch(error => {
+            console.log('Auth Service: signup error', error);
+            if (error.code)
+                return { isValid: false, message: error.message };
+        });
+}
+
+resetPassword(email: string): Promise<any> {
+    return this.afAuth.sendPasswordResetEmail(email)
+        .then(() => {
+            console.log('Auth Service: reset password success');
+            // this.router.navigate(['/amount']);
+        })
+        .catch(error => {
+            console.log('Auth Service: reset password error...');
+            console.log(error.code);
+            console.log(error)
+            if (error.code)
+                return error;
+        });
+}
+
+async resendVerificationEmail() {                         // verification email is sent in the Sign Up function, but if you need to resend, call this function
+    return (await this.afAuth.currentUser).sendEmailVerification()
+        .then(() => {
+            // this.router.navigate(['home']);
+        })
+        .catch(error => {
+            console.log('Auth Service: sendVerificationEmail error...');
+            console.log('error code', error.code);
+            console.log('error', error);
+            if (error.code)
+                return error;
+        });
+}
+
+logoutUser(): Promise<void> {
+    return this.afAuth.signOut()
+        .then(() => {
+            this.router.navigate(['/home']);                    // when we log the user out, navigate them to home
+        })
+        .catch(error => {
+            console.log('Auth Service: logout error...');
+            console.log('error code', error.code);
+            console.log('error', error);
+            if (error.code)
+                return error;
+        });
+}
+
+setUserInfo(payload: object) {
+    console.log('Auth Service: saving user info...');
+    this.afs.collection('users')
+        .add(payload).then(function (res) {
+            console.log("Auth Service: setUserInfo response...")
+            console.log(res);
+        })
+}
+
+getCurrentUser() {
+    return this.afAuth.currentUser;                                 // returns user object for logged-in users, otherwise returns null
+}
+      // END OF EMAIL SIGN IN
 
 }
