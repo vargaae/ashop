0f150ce530114496470b7d688f8be5b838c44110
vargaae <58937079+vargaae@users.noreply.github.com>
2021-09-08 14:09:48 +0200

    auth improved-navbar shows the username-logout fixed

--- src/app/core/components/bs-navbar/bs-navbar.component.ts
+++ src/app/core/components/bs-navbar/bs-navbar.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { AngularFireAuth } from '@angular/fire/auth';
+import { AngularFirestore } from '@angular/fire/firestore';
 import { Observable } from 'rxjs';
 
 import { fade, slide } from '../../../animations';
@@ -15,25 +17,41 @@ import { ShoppingCartService } from '../../../shared/services/shopping-cart.serv
   animations: [ fade, slide ]
 })
 export class BsNavbarComponent implements OnInit {
+    //EMAIL SIGNED IN USER
+  user: Observable<any>;              // Example: store the user's info here (Cloud Firestore: collection is 'users', docId is the user's email, lower case)
+    //GOOGLE SIGN IN APPUSER
   appUser: AppUser;
 
   cart$: Observable<ShoppingCart>;
 
   constructor(
+    private afAuth: AngularFireAuth, 
+    private firestore: AngularFirestore,
     public auth: AuthService,
     private shoppingCartService: ShoppingCartService
     ) {
-
+      this.user = null;
   }
 
+
   async ngOnInit() {
+    //EMAIL SIGNED IN USER
+    this.afAuth.authState.subscribe(user => {
+        console.log('Dashboard: user', user);
+
+        if (user) {
+            let emailLower = user.email.toLowerCase();
+            this.user = this.firestore.collection('users').doc(emailLower).valueChanges();
+        }
+    });
+    //GOOGLE SIGN IN APPUSER
+
     this.auth.appUser$
       .subscribe(appUser => this.appUser = appUser);
     this.cart$ = await this.shoppingCartService.getCart();
   }
 
   logout() {
-    this.auth.logout();
+    this.auth.logoutUser();
 }
-
 }
