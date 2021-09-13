18ccc4002dafc49edd126ab38edc30c769d590e0
vargaae <58937079+vargaae@users.noreply.github.com>
2021-09-13 09:56:23 +0200

    reset firebase deploy, styling

---
+++ src/environments/environment.ts
@@ -0,0 +1,26 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+  production: false,
+  firebase : {
+    apiKey: "AIzaSyCQsAMvRl5ulFlAuS-bclR-NXBoko5X3hE",
+    authDomain: "ashop-23e4c.firebaseapp.com",
+    databaseURL: "https://ashop-23e4c-default-rtdb.firebaseio.com",
+    projectId: "ashop-23e4c",
+    storageBucket: "ashop-23e4c.appspot.com",
+    messagingSenderId: "568629249485",
+    appId: "1:568629249485:web:54863d01d07ec37c57f084",
+    measurementId: "G-GJ5D28XVXY"
+  }
+};
+
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+// import 'zone.js/plugins/zone-error';  // Included with Angular CLI.
